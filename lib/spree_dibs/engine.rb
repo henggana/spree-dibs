67377d2c67ce51c35c876932dd2751764a108809
Segi Henggana <henggana7@gmail.com>
2015-11-06 13:30:01 +0700

    Fix undefined activate class method

--- lib/spree_dibs/engine.rb
+++ lib/spree_dibs/engine.rb
@@ -6,10 +6,16 @@ module SpreeDibs
 
     config.autoload_paths += %W(#{config.root}/lib)
 
-    initializer 'spree.gateway.payment_methods', after: 'spree.register.payment_methods' do |app|
-      app.config.spree.payment_methods << Spree::Gateway::Dibs
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
     end
 
     config.to_prepare &method(:activate).to_proc
+
+    initializer 'spree.gateway.payment_methods', after: 'spree.register.payment_methods' do |app|
+      app.config.spree.payment_methods << Spree::Gateway::Dibs
+    end
   end
 end
